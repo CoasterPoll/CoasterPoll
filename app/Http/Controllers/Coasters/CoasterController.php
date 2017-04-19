d9ce886c6467342aded5b4eb98908e71f1b9a4ef
Chase Hausman <chase@chaseh.net>
2017-04-18 20:29:01 -0700

    Coasters should have their own page.

---
+++ app/Http/Controllers/Coasters/CoasterController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace ChaseH\Http\Controllers\Coasters;
+
+use ChaseH\Models\Coasters\Coaster;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\Request;
+use ChaseH\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Cache;
+
+class CoasterController extends Controller
+{
+    public function view($pk, $cstr) {
+        if(Cache::has($pk.":".$cstr)) {
+            $coaster = Cache::get($pk.":".$cstr);
+        } else {
+            try {
+                $coaster = Coaster::where('slug', $cstr)->whereHas('park', function($query) use ($pk) {
+                    $query->where('parks.short', $pk);
+                })->with('park', 'categories', 'type', 'manufacturer')->firstOrFail();
+
+                Cache::put($pk.":".$cstr, $coaster, 120);
+            } catch (ModelNotFoundException $e) {
+                return abort(404);
+            }
+        }
+
+        return view('coasters.coaster', [
+            'coaster' => $coaster,
+        ]);
+    }
+}
