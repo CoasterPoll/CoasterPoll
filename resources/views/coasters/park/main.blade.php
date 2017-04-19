fb599f280a9cb822f07873907b92a2b80a4fe0ec
Chase Hausman <chase@chaseh.net>
2017-04-19 15:56:09 -0700

    Remove the unlikely scenario that a park doesn’t have any coasters to list.

--- resources/views/coasters/park/main.blade.php
+++ resources/views/coasters/park/main.blade.php
@@ -8,7 +8,9 @@
                 <h4 class="card-title">{{ $park->short }}</h4>
                 <h6 class="card-subtitle">{{ $park->city }}, {{ $park->country }}</h6>
                 <p class="card-text">{{ $park->copyright }}</p>
-                <a class="card-link" href="{{ $park->website }}">Website</a>
+                @if($park->website !== null)
+                    <a class="card-link" href="{{ $park->website }}">Website</a>
+                @endif
                 @if($park->rcdb_id !== null)
                     <a class="card-link" href="https://rcdb.com/{{ $park->rcdb_id }}.htm">View on RCDB</a>
                 @endif
@@ -18,19 +20,23 @@
     <div class="col-md-6">
         <div class="card">
             <div class="card-block">
-                <table class="table">
-                    <tbody>
-                    @foreach($park->coasters as $coaster)
-                        <tr>
-                            <th><a href="{{ route('coasters.coaster', ['park' => $park->short, 'coaster' => $coaster->slug]) }}" class="link-unstyled">{{ $coaster->name }}</a></th>
-                            <td><a href="{{ route('coasters.manufacturer', ['manufacturer' => $coaster->manufacturer->abbreviation]) }}">{{ $coaster->manufacturer->name }}</a></td>
-                            @can('Can track coasters')
-                                <td>@include('coasters._ridden', ['ridden_coaster_id' => $coaster->id])</td>
-                            @endcan
-                        </tr>
-                    @endforeach
-                    </tbody>
-                </table>
+                @if($park->coasters->count() > 0)
+                    <table class="table">
+                        <tbody>
+                        @foreach($park->coasters as $coaster)
+                            <tr>
+                                <th><a href="{{ route('coasters.coaster', ['park' => $park->short, 'coaster' => $coaster->slug]) }}" class="link-unstyled">{{ $coaster->name }}</a></th>
+                                <td><a href="{{ route('coasters.manufacturer', ['manufacturer' => $coaster->manufacturer->abbreviation]) }}">{{ $coaster->manufacturer->name }}</a></td>
+                                @can('Can track coasters')
+                                    <td>@include('coasters._ridden', ['ridden_coaster_id' => $coaster->id])</td>
+                                @endcan
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                @else
+                    <p class="lead"><em>They don't have any coasters. :'(</em></p>
+                @endif
             </div>
         </div>
     </div>
