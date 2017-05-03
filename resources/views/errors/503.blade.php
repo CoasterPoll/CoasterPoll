ff35044db4a648e68949cebb989e7aea137efd10
Chase Hausman <chase@chaseh.net>
2017-05-02 17:45:14 -0700

    Remove use of env().

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -11,7 +11,7 @@
         <div class="col-8 offset-2 text-center mt-4 pt-4">
             <h1 class="display-1 mt-4 pt-4 hidden-xs-down">Sorry Folks...</h1>
             <h1 class="display-4 mt-4 pt-4 hidden-sm-up">Sorry Folks...</h1>
-            <h1 class="lead">We're closed to clean and repair {{ env('APP_NAME') }}.</h1>
+            <h1 class="lead">We're closed to clean and repair {{ config('app.name') }}.</h1>
             <p>We'll try and be fast. Please don't bring a gun.</p>
         </div>
     </div>
