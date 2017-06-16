d3e1cecad9126caa1c642cd3bb6ff88c4138265f
Chase Hausman <chase@chaseh.net>
2017-06-16 16:04:52 -0700

    Link to comments and profiles.

--- resources/views/sharing/partials/_comment.blade.php
+++ resources/views/sharing/partials/_comment.blade.php
@@ -3,18 +3,21 @@
         foreach($comments as $comment):
 
 ?>
-    <div class="card-block comment" data-comment="{{ $comment->id }}">
+    <div class="card-block comment" id="comment{{ $comment->getId() }}" data-comment="{{ $comment->id }}">
         <h4>
-            {{ $comment->user->handle ?? "[DELETED]" }}
+            <a href="{{ $comment->user->getProfileLink() }}" class="lead-unstyled">{{ $comment->user->handle ?? "[DELETED]" }}</a>
             @if($comment->parent_id ?? false)
-                <small>in reply to {{ $comment->parent->user->handle }}</small>
+                <small>in reply to <a href="{{ $comment->parent->user->getProfileLink() }}">{{ $comment->parent->user->handle }}</a></small>
             @endif
         </h4>
         <p class="card-text">{{ $comment->body ?? "Comment Not Found" }}</p>
-        <a class="card-link small" href="#{{ $comment->getId() }}">Permalink</a>
+        <a class="card-link small" href="#comment{{ $comment->getId() }}">Permalink</a>
         @can('Can comment')
             <a class="card-link small reply-to-comment" role="button">Reply</a>
         @endcan
+        @auth
+            <a class="card-link small" role="button" id="report-{{ $comment->getId() }}">Report</a>
+        @endauth
         @if($comment->children ?? false)
             <?php $traverse($comment->children) ?>
         @endif
