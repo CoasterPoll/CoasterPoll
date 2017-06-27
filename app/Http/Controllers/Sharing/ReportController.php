df9cb477bd2d778aeae5742425ed10cf43468170
Chase Hausman <chase@chaseh.net>
2017-06-27 14:16:29 -0700

    Beginning to allow reporting links.

---
+++ app/Http/Controllers/Sharing/ReportController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace ChaseH\Http\Controllers\Sharing;
+
+use Illuminate\Http\Request;
+use ChaseH\Http\Controllers\Controller;
+
+class ReportController extends Controller
+{
+    //
+}
